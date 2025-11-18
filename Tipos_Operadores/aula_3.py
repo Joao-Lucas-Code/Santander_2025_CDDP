0d578d7133dc03ee26877333b156d9119ab69d7f
João Lucas <joao.lgomes1166@gmail.com>
2025-11-18 13:41:47 -0300

    feat: operadores logicos

---
+++ Tipos_Operadores/aula_3.py
@@ -0,0 +1,10 @@
+saldo = 1000
+saque = 200
+limete = 200 
+conta_especial = True
+
+print(True and True)
+print(True and False)
+
+saldo >= saque and conta_especial and saque > limete 
+print(saldo >= saque and conta_especial and saque > limete )
