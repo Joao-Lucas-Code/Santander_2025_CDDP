276e877a02725f62b3e6f6b41ea8f3331234f34d
João Lucas <joao.lgomes1166@gmail.com>
2025-11-25 11:29:18 -0300

    feat: conhecendo metodos uteis da classe string

---
+++ Strings_e_Fatiamento/string_1.py
@@ -0,0 +1,8 @@
+nome = 'jolito'
+
+print(nome[0])    # 'j'
+print(nome.upper())  # 'JOLITO'
+print(nome.lower())  # 'jolito'
+print(nome.title())  # 'Jolito'
+
+print(nome.strip() + ".")  # 'jolito' (removes leading/trailing whitespace)
