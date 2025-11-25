bfa4a4be99f9595c6960890849b91146f071d186
João Lucas <joao.lgomes1166@gmail.com>
2025-11-25 11:35:32 -0300

    feat: fatiamento de string

---
+++ Strings_e_Fatiamento/string_3.py
@@ -0,0 +1,9 @@
+nome = "Caua Mendes Pinto"
+
+print(nome[0:4])        # 'Caua'
+print(nome[5:12])       # 'Mendes '
+print(nome[:4])         # 'Caua'
+print(nome[5:])         # 'Mendes Pinto'
+print(nome[-5:])        # 'Pinto'
+print(nome[::2])        # 'Ca e pi o' (every second character)
+print(nome[::-1])       # 'otniP sedneM auaC' (re
