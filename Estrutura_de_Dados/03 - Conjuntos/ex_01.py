dd271f3b86649d062fdfe1e1718125b60adc1144
João Lucas <joao.lgomes1166@gmail.com>
2025-12-02 10:44:35 -0300

    feat: exercicio 1 conjuntos

---
+++ Estrutura_de_Dados/03 - Conjuntos/ex_01.py
@@ -0,0 +1,4 @@
+print(set(['1', '2', '3']))  # Cria um conjunto com os elementos '1', '2' e '3'
+print(set("abacaxi"))
+print(set(("palio", "celta", "uno")))  # Cria um conjunto com os elementos 'palio', 'celta' e 'uno'
+print(set({'chave1': 'valor1', 'chave2': 'valor2'}))
