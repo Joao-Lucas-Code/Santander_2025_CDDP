39308a86b291a3759c6dd756cd70d3d02c516133
João Lucas <joao.lgomes1166@gmail.com>
2025-12-03 09:48:20 -0300

    feat: primeiro dicionario

---
+++ Estrutura_de_Dados/04 - Dicionarios/ex_01.py
@@ -0,0 +1,12 @@
+contatos = {
+  "João": {"telefone": "1234-5678", "email": "joao@example.com"},
+  "Maria": {"telefone": "8765-4321", "email": "maria@example.com"},
+  "Caua": {"telefone": "1122-3344", "email": "caua@example.com"},
+  "Maikon": {"telefone": "9988-7766", "email": "maikon@example.com"}
+}
+
+telefone_joao = contatos["João"]["telefone"]
+print(f"O telefone do João é: {telefone_joao}")
+
+print(set(['1', '2', '3']))  # Cria um conjunto com os elementos '1', '2' e '3'
+print(set("abacaxi"))
