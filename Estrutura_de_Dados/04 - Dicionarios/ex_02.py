c6a10e429e836a9b39395c8236f4050eef3f80e5
João Lucas <joao.lgomes1166@gmail.com>
2025-12-03 10:06:36 -0300

    feat: usando o update

---
+++ Estrutura_de_Dados/04 - Dicionarios/ex_02.py
@@ -0,0 +1,8 @@
+contatos = {
+    "Alice": {"telefone": "5555-1234", "email": "alice@example.com"}
+}
+
+contatos.update({
+    "Bob": {"telefone": "5555-5678", "email": "bob@example.com"}})
+
+print(contatos)
