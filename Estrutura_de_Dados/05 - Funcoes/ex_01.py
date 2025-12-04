fc207121cd5115972bbd1dc199b7cc029021453f
João Lucas <joao.lgomes1166@gmail.com>
2025-12-04 09:17:24 -0300

    feat: primeira funcao

---
+++ Estrutura_de_Dados/05 - Funcoes/ex_01.py
@@ -0,0 +1,9 @@
+def exibir_mensagem():
+    print("Olá, Mundo!")
+
+def adicionar_numeros(a, b):
+    return a + b
+
+exibir_mensagem()
+resultado = adicionar_numeros(5, 7)
+print("A soma é:", resultado)
